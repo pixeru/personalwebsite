4317f99853ef73ee5f4a9b6719686c4a22fded2a
ピクセルGM <54094844+Akiraa93@users.noreply.github.com>
2024-08-22 22:02:40 -0700

    Removed Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ function RenderAbout() {
                                     <span className="block">joshuaprayogo at gmail.com</span> */}
                                 </p>
                             </div>
-                            <div className="w-full md:w-1/2">
+                            {/* <div className="w-full md:w-1/2">
                                 <p className="mb-2">
                                     <a href="./CV_Joshua_Prayogo.pdf" target="_blank" className="button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded inline-flex items-center">
                                         <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
@@ -77,7 +77,7 @@ function RenderAbout() {
                                         <span>Download Resume</span>
                                     </a>
                                 </p>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                 </div>
